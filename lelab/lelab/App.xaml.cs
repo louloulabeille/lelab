ae4a3d88817e4014de53d1ca01976fa3a8a11bca
louloulabeille <louloulabeille@hotmail.com>
2023-10-25 17:37:19 +0200

    Ajoutez des fichiers projet.

---
+++ lelab/lelab/App.xaml.cs
@@ -0,0 +1,32 @@
+﻿using lelab.Services;
+using lelab.Views;
+using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace lelab
+{
+    public partial class App : Application
+    {
+
+        public App()
+        {
+            InitializeComponent();
+
+            DependencyService.Register<MockDataStore>();
+            MainPage = new CountPage();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ lelab/lelab/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using lelab.ViewModels;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace lelab.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
