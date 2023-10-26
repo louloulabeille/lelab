717ddee8ce49baac4d3f3c0aaa0d353291fb3f8c
louloulabeille <louloulabeille@hotmail.com>
2023-10-26 16:05:26 +0200

    Modification de l'animation pour qu'elle se fasse une après les autres

--- lelab/lelab/Views/TabAvion.xaml.cs
+++ lelab/lelab/Views/TabAvion.xaml.cs
@@ -20,7 +20,7 @@ namespace lelab.Views
 
         protected override void OnAppearing()
         {
-            uint timeLength = 800;
+            //uint timeLength = 800;
 
             base.OnAppearing();
 
@@ -29,12 +29,26 @@ namespace lelab.Views
 			avionImage.Rotation = 45;
 			avionImage.Scale = 0.7;
 			avionImage.Opacity = 0;
-			// translation
-			avionImage.FadeTo(1, 100);
+			// translation elles se font toute en même temps
+			/*avionImage.FadeTo(1, 100);
 			avionImage.TranslateTo(0, 0, timeLength, Easing.SinOut);
 			avionImage.RotateTo(0, timeLength, Easing.SinOut);
-			avionImage.ScaleTo(1,timeLength,Easing.SinOut);
+			avionImage.ScaleTo(1,timeLength,Easing.SinOut);*/
+			Amin();
+        }
 
+		/// <summary>
+		/// animation qui se font une après les autres
+		/// </summary>
+		/// <returns></returns>
+		private async void Amin()
+		{
+            uint timeLength = 800;
+
+            await avionImage.FadeTo(1, 100);
+            await avionImage.TranslateTo(0, 0, timeLength, Easing.SinOut);
+            await avionImage.RotateTo(0, timeLength, Easing.SinOut);
+            await avionImage.ScaleTo(1, timeLength, Easing.SinOut);
         }
     }
 }
